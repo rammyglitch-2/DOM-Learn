0b8c759e1ca78c07bdd5a94b6e34396cd15fe49a
rammyglitch-2 <Rammypius3@gmail.com>
2026-09-14 20:49:55 +0100

    new changes

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-// Get Element byID
-let title_Element = document.getelementbyid('store-title');
-console.log(title_Element); // Return the h1 element
-console.log(title_Element.textcontent);// TechZone Store
+// Get Element by ID
+let titleElement = document.getElementById('store-title');
+console.log(titleElement); // Returns the h1 element
+console.log(titleElement.textContent); // TechZone Store
 
-//query selector
-let product_Element = document.querySelector('#products-section');
-let first_productcard = document.querySelector('.product-name');
-console.log(product_Element); // return the first div of the section
-console.log(first_productcard); // return the first h3 of the div
+// Query selector
+let productElement = document.querySelector('#products-section');
+let firstProductCard = document.querySelector('.product-name');
+console.log(productElement); // Returns the first div of the section
+console.log(firstProductCard); // Returns the first h3 of the div
 
-// query selector all
-let allproductprices = document.queryselectorall('prices'); 
-let allproductnames = document.querySelectorAll('product-names'); 
-let allproductcards = document.queryselectorall('product-card'); 
+// Query selector all
+let allProductPrices = document.querySelectorAll('.price');
+let allProductNames = document.querySelectorAll('.product-name');
+let allProductCards = document.querySelectorAll('.product-card');
 console.log('Number of products:', allProductCards.length);
-console.log( 'Number of names:', allproductnames.length)
-console.log('NUmber of prices:',allproductprices.length)
+console.log('Number of names:', allProductNames.length);
+console.log('Number of prices:', allProductPrices.length);
 
 // Elements by class name
+let productCards = document.getElementsByClassName('product-card');
+console.log('product cards:', productCards.length);
 
+// Element by tag
+let all_Elements = dovument.getElementsbytagname('h3');
+console.log('all_Element:',h3.length);
